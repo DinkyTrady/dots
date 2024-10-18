407f99300d7db6517c40be6e4e09ef35506ee6bb
DinkyTrady <DinkyTrady@kyra.com>
2024-10-18 21:21:39 +0700

    init

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+
+    # Set variable
+    set -Ux SDKMAN_DIR $HOME/DevPkg/.sdkman
+    set -U nvm_default_version v20.18.0
+    set -U nvm_default_packages npm
+
+    set -Ux RUSTUP_HOME $HOME/DevPkg/.rustup
+    set -Ux CARGO_HOME $HOME/DevPkg/.cargo
+
+    alias vi nvim
+    set -Ux EDITOR nvim
+
+    # Add path
+    fish_add_path ~/.local/share/nvim/mason/bin
+end
---
+++ README.md
@@ -0,0 +1,27 @@
+# Dotsfiles!
+
+### How To Install
+
+#### Package required
+
+1. git
+2. stow
+
+#### install git
+
+`sudo pacman -S git`
+
+#### install stow
+
+`sudo pacman -S stow`
+
+#### installing dotsfiles
+
+> [!WARNING]
+> Remember to backup folder before installing
+
+```sh
+git clone --depth 1 https://github.com/DinkyTrady/dots.git
+cd ./dots
+stow .
+```
